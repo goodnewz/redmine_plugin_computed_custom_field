0e4718a83850d6eed554e17f703fa4daab0727d7
goodnewz <cvet84@gmail.com>
2024-10-05 01:30:45 -0400

    Redmine 5.1 compatibility in init file

--- init.rb
+++ init.rb
@@ -1,22 +1,40 @@
+require 'redmine'
+
 Redmine::Plugin.register :computed_custom_field do
   name 'Computed custom field'
   author 'Yakov Annikov'
   url 'https://github.com/annikoff/redmine_plugin_computed_custom_field'
   description ''
-  version '1.0.7'
+  version '1.0.8'
   settings default: {}
 end
 
-$LOAD_PATH.unshift "#{File.dirname(__FILE__)}/lib"
-require 'computed_custom_field'
-#require 'computed_custom_field/custom_field_patch'
-require 'computed_custom_field/custom_fields_helper_patch'
-#require 'computed_custom_field/model_patch'
-#require 'computed_custom_field/issue_patch'
-require 'computed_custom_field/hooks'
+require_relative 'lib/computed_custom_field/custom_field_patch'
+require_relative 'lib/computed_custom_field/custom_fields_helper_patch'
+require_relative 'lib/computed_custom_field/model_patch'
+require_relative 'lib/computed_custom_field/issue_patch'
+require_relative 'lib/computed_custom_field/hooks'
 
 RedmineApp::Application.configure do
   config.after_initialize do
-    ComputedCustomField.patch_models
+    models = [
+      Enumeration, Group, Issue, Project,
+      TimeEntry, User, Version
+    ]
+    models.each do |model|
+      if model.included_modules
+              .exclude?(ComputedCustomField::ModelPatch)
+        model.send :include, ComputedCustomField::ModelPatch
+      end
+    end
   end
+end
+
+if Rails::VERSION::MAJOR >= 5
+  version = "#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}".to_f
+  PLUGIN_MIGRATION_CLASS = ActiveRecord::Migration[version]
+  preparation_class = ActiveSupport::Reloader
+else
+  PLUGIN_MIGRATION_CLASS = ActiveRecord::Migration
+  preparation_class = ActionDispatch::Callbacks
 end
